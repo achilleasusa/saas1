c3982d2120459475a521bb3b3dec5f89e36aceb5
achilleas <akarayianidis@gmail.com>
2019-01-26 14:38:27 -0500

    Add elasticSearch feature

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react'
+import { ReactiveBase, DataSearch, CategorySearch } from '@appbaseio/reactivesearch';
 const Searchbar = () => {
   return(
     <div>
-      Searchbar
-    </div>
+			<ReactiveBase
+        app="index"
+        type="type"
+        url="http://localhost:9200"
+      >
+      <DataSearch
+        componentId="componentId"
+        dataField="title"
+        // autosuggest={true}
+        // fieldWeights={[1, 3]}
+        // highlight={true}
+        // showFilter={true}
+        fuzziness={1}
+      />
+      {/* <CategorySearch
+        componentId="searchbox"
+        dataField="model"
+        categoryField="brand.keyword"
+        placeholder="Search for cars"
+      />  */}
+			</ReactiveBase>
+      </div>
   )
 }
 export default Searchbar
